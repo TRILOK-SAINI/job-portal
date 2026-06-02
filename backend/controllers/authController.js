a07defe9c800a57cb058db43fed996fa99798188
Trilok saini <trilok2962005@gmail.com>
2026-06-02 19:03:03 +0530

    maxage of cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -76,6 +76,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: isProduction,
       sameSite: isProduction ? "none" : "lax",
+       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.json({
